2e6393c6febcc980f9cf9c86c4412eaba1fa3dfd
JordanWheelerDev <163913618+JordanWheelerDev@users.noreply.github.com>
2024-07-08 15:42:55 -0230

    Create ToS and Privacy Policy

---
+++ privacy-policy.php
@@ -0,0 +1,135 @@
+<?php
+include 'functions.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DiscHub | Terms of Service</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+
+<body>
+    <?php include "parts/navbar.php"; ?>
+    <div class="container mt-5 mb-5">
+        <div class="row mb-4 d-flex justify-content-center">
+            <div class="col-md-8">
+                <div class="ds-header-l mb-4">Privacy Policy</div>
+                <div class="card ds-card rounded-0">
+                    <div class="card-body">
+                        <div class="mb-3 text-center text-sm">Last updated: July 8th, 2024.</div>
+                        <div class="mb-2 text-xl">
+                            1. Introduction
+                        </div>
+                        <div class="mb-4">
+                            Welcome to DiscHub! This Privacy Policy explains how DiscHub collects, uses, discloses, and
+                            protects
+                            your information. By using DiscHub, you consent to the practices described in this policy.
+                        </div>
+                        <div class="mb-2 text-xl">
+                            2. Information Collection and Use
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Information You Provide</b>: When you register or use DiscHub, we may collect
+                                    personal
+                                    information such as your name, email address, and Discord server details.</li>
+                                <li><b>Automatically Collected Information</b>: We may collect information about your
+                                    device and
+                                    usage patterns, including IP address, browser type, and cookies.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            3. Use of Information
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Personalization</b>: To personalize your experience and improve our services.
+                                </li>
+                                <li><b>Communication</b>: To communicate with you, respond to inquiries, and send
+                                    updates.</li>
+                                <li><b>Analytics</b>: To analyze usage trends and improve DiscHub's functionality.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            4. Information Sharing
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Third-Party Services</b>: We may share your information with third-party service
+                                    providers
+                                    for operational purposes.</li>
+                                <li><b>Legal Compliance</b>: We may disclose information when required by law or to
+                                    protect our
+                                    rights.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            5. Data Security
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Security Measures</b>: We implement industry-standard security measures to
+                                    protect your
+                                    information.</li>
+                                <li><b>Data Breach</b>: In the event of a data breach, we will notify affected users
+                                    promptly.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            6. Your Choices
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Access and Update</b>: You can access and update your personal information
+                                    through your
+                                    DiscHub account settings.</li>
+                                <li><b>Cookies</b>: You can manage cookies through your browser settings.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            7. Contact Us
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Questions</b>: If you have any questions about this Privacy Policy, please
+                                    contact us at
+                                    info@dischub.org.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            8. Changes to This Policy
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Updates</b>: We may update this Privacy Policy from time to time. We will notify
+                                    you of any
+                                    changes by posting the new policy on DiscHub.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            9. Governing Law
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Jurisdiction</b>: This Privacy Policy shall be governed by and construed in
+                                    accordance with
+                                    the laws of Newfoundland and Labrador, Canada.</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ tos.php
@@ -0,0 +1,169 @@
+<?php
+include 'functions.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DiscHub | Terms of Service</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="css/all.min.css">
+</head>
+
+<body>
+    <?php include "parts/navbar.php"; ?>
+    <div class="container mt-5 mb-5">
+        <div class="row mb-4 d-flex justify-content-center">
+            <div class="col-md-8">
+                <div class="ds-header-l mb-4">Terms of Service</div>
+                <div class="card ds-card rounded-0">
+                    <div class="card-body">
+                        <div class="mb-3 text-center text-sm">Last updated: July 8th, 2024.</div>
+                        <div class="mb-2 text-xl">
+                            1. Introduction
+                        </div>
+                        <div class="mb-4">
+                            Welcome to DiscHub! These Terms of Service govern your use of the DiscHub website and
+                            services provided therein. By accessing or using DiscHub, you agree to comply with these
+                            terms.
+                        </div>
+                        <div class="mb-2 text-xl">
+                            2. Definitions
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>DiscHub</b>: Refers to the DiscHub website and services provided by DiscHub.</li>
+                                <li><b>User</b>: Any person who accesses or uses DiscHub, including Discord server
+                                    owners listing their servers.</li>
+                                <li><b>Content</b>: Refers to text, images, videos, links, or any other material posted
+                                    on DiscHub.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            3. User Responsibilities
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Accurate Information</b>: Users must provide accurate and truthful information
+                                    when listing their Discord servers on DiscHub.</li>
+                                <li><b>Compliance</b>: Users must comply with all applicable laws and regulations.</li>
+                                <li><b>Prohibited Content</b>: Users must not post, upload, or share any content that
+                                    includes, but is not limited to: <ul>
+                                        <li><b>Violence</b>: Content that promotes violence, threats, or harm against
+                                            individuals or groups.</li>
+                                        <li><b>Drugs and Substance Abuse</b>: Content that promotes or glorifies drug
+                                            use, substance abuse, or illegal activities related to controlled
+                                            substances.</li>
+                                        <li><b>Underage Content</b>: Content that depicts, promotes, or encourages
+                                            illegal or inappropriate activities involving minors, including but not
+                                            limited to underage drinking, pornography, or exploitation.</li>
+                                        <li><b>Hate Speech</b>: Content that promotes discrimination, hatred, or
+                                            violence against individuals or groups based on race, ethnicity, religion,
+                                            gender, sexual orientation, disability, or any other protected
+                                            characteristic.</li>
+                                        <li><b>Illegal Activities</b>: Content that promotes or facilitates illegal
+                                            activities or violates the rights of others.</li>
+                                    </ul>
+                                </li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            4. DiscHub Services
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Server Listings</b>: DiscHub allows Discord server owners to list their servers
+                                    and manage their listings.</li>
+                                <li><b>Bumping</b>: Servers can be bumped to the top of their respective categories
+                                    every 2 hours by their owners and users.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            5. Limitation of Liability
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>No Warranty</b>: DiscHub provides its services on an "as-is" and "as-available"
+                                    basis without warranties of any kind.
+                                    and manage their listings.</li>
+                                <li><b>Limitation</b>: In no event shall DiscHub be liable for any indirect, incidental,
+                                    special, consequential, or punitive damages.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            6. User Conduct
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Prohibited Activities</b>: Users must not engage in spamming, phishing, hacking,
+                                    or any activity that disrupts or interferes with DiscHub’s services..</li>
+                                <li><b>Moderation</b>: DiscHub reserves the right to moderate and remove any content
+                                    that
+                                    violates these terms or is deemed inappropriate.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            7. Intellectual Property
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Ownership</b>: Users retain ownership of their content posted on DiscHub.</li>
+                                <li><b>License</b>: By posting content on DiscHub, users grant DiscHub a
+                                    non-exclusive, royalty-free, worldwide license to use, reproduce, modify, adapt,
+                                    publish, translate, create derivative works from, distribute, and display such
+                                    content.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            8. Privacy Policy
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Collection of Information</b>: DiscHub collects and uses information as outlined
+                                    in its Privacy Policy.</li>
+                                <li><b>Cookies</b>: By using DiscHub, users consent to the use of cookies in accordance
+                                    with the Privacy Policy.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            9. Termination
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Termination</b>: DiscHub reserves the right to suspend or terminate a user’s
+                                    access to its services at any time for violations of these terms.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            9. Amendments
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Updates</b>: DiscHub may update these Terms of Service from time to time. Users
+                                    will be notified of any changes.</li>
+                            </ol>
+                        </div>
+                        <div class="mb-2 text-xl">
+                            10. Governing Law
+                        </div>
+                        <div class="mb-4">
+                            <ol>
+                                <li><b>Jurisdiction</b>: These Terms of Service shall be governed by and construed in
+                                    accordance with the laws of Newfoundland and Labrador, Canada.</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
